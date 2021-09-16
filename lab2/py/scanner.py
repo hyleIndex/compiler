b7eb5084bd822a7e871f57563fce7b305961ecea
hyleIndex <ypxrain@outlook.com>
2021-09-16 19:15:46 +0200

    Update scanner.py

--- lab2/py/scanner.py
+++ lab2/py/scanner.py
@@ -1,51 +1,131 @@
-import re
-import ply.lex as lex
-import ply.yacc as yacc
+import getopt, sys
+from ply import lex as lex
+from ply.lex import TOKEN
 
-reserved = {'print': 'PRINT', 'while': 'WHILE', 'def': 'def'}
+class Lexer(object):
+    def __init__(self, **kwargs):
+        self.lexer = lex.lex(object=self, **kwargs)
 
-tokens = (
-    'PLUS', 'MINUS', 'SEMICOLON', 'LPAREN', 'RPAREN', 'IDENT', 'NUMBER',
-    'BITSHL', 'BITSHR', 'BITXOR', 'BITOR', 'BITAND', 
-    'TIMES', 'DIV', 'MODULUS', 'UMINUS', 'BITCOMPL',
-    'ASSIGN'
-) + tuple(reserved.values())
+    def input(self, text):
+        self.lexer.input(text)
+    
+    def reset_lineno(self):
+        self.lexer.lineno = 1
+        
+    def token(self):
+        self.last_token = self.lexer.token()
+        return self.last_token
+    
+    def find_tok_column(self, token):
+        """ Find the column of the token in its line.
+        """
+        last_cr = self.lexer.lexdata.rfind('\n', 0, token.lexpos)
+        return token.lexpos - last_cr
 
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_PLUS = r'\+'
-t_MINUS = '-'
-t_SEMICOLON = ';'
-t_BITSHL = r'\>\>'
-t_BITSHR = r'\<\<'
-t_BITXOR = r'\^'
-t_BITOT = r'|'
-t_BITAND = r'\&'
-t_TIMES = r'*'
-t_DIV = r'\/'
-t_MODULUS = r'\%'
-t_BITCOMPL = r'\~'
-t_ASSIGN = r'\='
+    # Test it output
+    def test(self, data):
+        self.input(data)
+        while True:
+            tok = self.token()
+            if tok:
+                print(tok)
+            else:
+                break
 
-def t_IDENT(t):
-    r'[A-Za-z_][A-Za-z0-9_]*'
-    t.type = reserved.get(t.value, 'IDENT')
-    return t
+    ## PRIVATE ##
 
-def t_NUMBER(t):
-    r'[1-0][0-9]*'
-    t.value = int(t.value)
-    return t
+    keywords = {'print': 'PRINT', 'while': 'WHILE', 'def': 'DEF'}
 
-def t_error(t):
-    print(f”Illegalcharacter’{t.value[0]}’online{t.lexer.lineno}”)
-    t.lexer.skip(1)
+    tokens = (
+        'PLUS', 'MINUS', 'SEMICOLON', 'LPAREN', 'RPAREN', 'IDENT', 'NUMBER',
+        'BITSHL', 'BITSHR', 'BITXOR', 'BITOR', 'BITAND', 
+        'TIMES', 'DIV', 'MODULUS', 'UMINUS', 'BITCOMPL',
+        'ASSIGN'
+    ) + tuple(keywords.values())
 
-t_ignore = ' \t\f\v'
+    identifier = r'[a-zA-Z_][0-9a-zA-Z_]*'
+    t_DEF = r'def'
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+    t_PLUS = r'\+'
+    t_MINUS = '-'
+    t_SEMICOLON = ';'
+    t_BITSHL = r'\>\>'
+    t_BITSHR = r'\<\<'
+    t_BITXOR = r'\^'
+    t_BITOR = r'\|'
+    t_BITAND = r'\&'
+    t_TIMES = r'\*'
+    t_DIV = r'\/'
+    t_MODULUS = r'\%'
+    t_BITCOMPL = r'\~'
+    t_ASSIGN = r'\='
 
-def t_newline(t):
-    r'\n'
-    t.lexer.lineno += 1
+    # Ignored characters
+    t_ignore = " \t:"
+    
+    @TOKEN(identifier)
+    def t_IDENT(self, t):
+        if t.value in self.keywords:
+            t.type = self.keywords[t.value]
+        return t
 
-def t_program(t):
-    r'def main() {}'
+    def t_NUMBER(self, t):
+        r'[0-9][0-9]*'
+        t.value = int(t.value)
+        return t
+
+
+    # character constants (K&R2: A.2.5.2)
+    # Note: a-zA-Z and '.-~^_!=&;,' are allowed as escape chars to support #line
+    # directives with Windows paths as filenames (..\..\dir\file)
+    # For the same reason, decimal_escape allows all digit sequences. We want to
+    # parse all correct code, even if it means to sometimes parse incorrect
+    # code.
+    #
+    simple_escape = r"""([a-zA-Z._~!=&\^\-\\?'"])"""
+    decimal_escape = r"""(\d+)"""
+    hex_escape = r"""(x[0-9a-fA-F]+)"""
+    bad_escape = r"""([\\][^a-zA-Z._~^!=&\^\-\\?'"x0-7])"""
+
+    escape_sequence = r"""(\\("""+simple_escape+'|'+decimal_escape+'|'+hex_escape+'))'
+    cconst_char = r"""([^'\\\n]|"""+escape_sequence+')'
+    char_const = "'"+cconst_char+"'"
+    wchar_const = 'L'+char_const
+    unmatched_quote = "('"+cconst_char+"*\\n)|('"+cconst_char+"*$)"
+    bad_char_const = r"""('"""+cconst_char+"""[^'\n]+')|('')|('"""+bad_escape+r"""[^'\n]*')"""
+
+    # string literals (K&R2: A.2.6)
+    string_char = r"""([^"\\\n]|"""+escape_sequence+')'
+    string_literal = '"'+string_char+'*"'
+    wstring_literal = 'L'+string_literal
+    bad_string_literal = '"'+string_char+'*?'+bad_escape+string_char+'*"'
+
+    @TOKEN(char_const)
+    def t_CHAR_CONST(self, t):
+        return t
+
+    @TOKEN(string_literal)
+    def t_NORMALSTRING(self, t):
+        return t
+    
+    def t_COMMENTS(self, t):
+        r'\/\/.*\n*'
+        pass
+
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += t.value.count("\n")
+
+    def t_error(self, t):
+        print("Illegal character '%s'" % t.value[0])
+        t.lexer.skip(1)
+
+if __name__ == "__main__":
+    opts, args = getopt.getopt(sys.argv[1:], '', [])
+    with open(args[0], 'r') as fp:
+        data = fp.read()
+
+    print(data)
+    lexer = Lexer()
+    lexer.test(data)
